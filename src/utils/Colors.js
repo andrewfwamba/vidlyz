8c16d8d43800f26ccbdfe0348b6e62bc0f149bc0
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-01-30 22:10:36 +0300

    configs for colors

---
+++ src/utils/Colors.js
@@ -0,0 +1,9 @@
+export default Colors = [
+  {
+    primary: "#000",
+    transparentbg: "rgba(122, 111, 228, 0.2)",
+    mainbg: "#f0f0f7",
+    lightText: "#f4f4f7",
+    darkText: "#0E0849",
+  },
+];
