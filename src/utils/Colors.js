f8ac8b0798241a11c668057aa00a5839ec18a1f2
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-01-31 16:33:16 +0300

    debug

--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -1,11 +1,8 @@
-export const Colors = [
-  {
-    primary: "#000",
-    transparentbg: "rgba(122, 111, 228, 0.2)",
-    mainbg: "#f0f0f7",
-    lightText: "#f4f4f7",
-    darkText: "#0E0849",
-    iconcolor: "#AE1E15",
-  },
-];
-// console.log(Colors.lightText);
+export const Colors = {
+  primary: "#000",
+  transparentbg: "rgba(122, 111, 228, 0.2)",
+  mainbg: "#f0f0f7",
+  lightText: "#f4f4f7",
+  darkText: "#0E0849",
+  iconColor: "#AE1E15",
+};
