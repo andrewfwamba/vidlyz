61ee23ab8e869bcfaa92e9b56c113b86a4741ed7
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-01-31 11:30:31 +0300

    Added colors

--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -1,9 +1,11 @@
-export default Colors = [
+export const Colors = [
   {
     primary: "#000",
     transparentbg: "rgba(122, 111, 228, 0.2)",
     mainbg: "#f0f0f7",
     lightText: "#f4f4f7",
     darkText: "#0E0849",
+    iconcolor: "#AE1E15",
   },
 ];
+// console.log(Colors.lightText);
