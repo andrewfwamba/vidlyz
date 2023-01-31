ffc0ec276456f0f8c241af0b660d7453e15b3bbc
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-01-31 23:02:32 +0300

    added route

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/screens/Home";
 import Login from "./src/screens/Login";
 import Signup from "./src/screens/Signup";
 
@@ -15,6 +16,7 @@ export default function App() {
       >
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
