a79aa108aae0edf1b13eb276235cb3fc4081061c
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-01-31 11:29:14 +0300

    New compoments

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,19 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import Colors from "../utils/Colors";
+import { Entypo } from "@expo/vector-icons";
 
 const Home = () => {
   return (
     <View style={styles.container}>
       {/* Header section */}
       <View style={{ height: 50, backgroundColor: "#000" }}>
+        <Entypo
+          name="folder-video"
+          size={50}
+          color={Colors.iconColor}
+          style={{ marginTop: 60, marginBottom: 0 }}
+        />
         <Text style={{ color: Colors.lightText }}>Vidlyz</Text>
       </View>
       {/* Label for video page */}
