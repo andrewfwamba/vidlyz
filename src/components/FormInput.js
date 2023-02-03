99cdccdb8b281d479ed98d308281578a496d327f
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-02-03 22:33:02 +0300

    modified props

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,29 +1,43 @@
 import { View, Text, StyleSheet, TextInput } from "react-native";
 import React from "react";
+import { Colors } from "../utils/Colors";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const FormInput = (props) => {
-  const { placeholder, label } = props;
+  const { placeholder, iconName } = props;
   return (
-    <>
-      <Text style={{ fontWeight: "bold" }}>{label}</Text>
-      <TextInput {...props} placeholder={placeholder} style={styles.input} />
-    </>
+    <View style={styles.inputWrapper}>
+      <Icon name={iconName} style={{ color: "gray", fontSize: 22 }} />
+      <TextInput
+        autoCorrect={false}
+        {...props}
+        placeholder={placeholder}
+        style={styles.input}
+      />
+    </View>
   );
 };
 
 export default FormInput;
 
 const styles = StyleSheet.create({
+  inputWrapper: {
+    width: "85%",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    borderRadius: 10,
+    borderWidth: 0.6,
+    marginTop: 5,
+    borderColor: "rgba(155, 146, 220, .3)",
+  },
   input: {
     height: 40,
     width: "85%",
-    backgroundColor: "rgba(255, 255, 255, 0.4)",
     padding: 5,
     borderRadius: 10,
     fontSize: 15,
     fontWeight: "700",
-    marginBottom: 20,
-    borderWidth: 0.6,
-    borderColor: "rgba(155, 146, 220, .3)",
   },
 });
