e1d7a737c2b900269343ce1f3314737b578a3ed8
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-02-11 22:01:42 +0300

    more colors

--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -1,8 +1,10 @@
 export const Colors = {
   primary: "#000",
-  transparentbg: "rgba(122, 111, 228, 0.2)",
+  transparentbg: "rgba(0,0,0,0.2)",
   mainbg: "#f0f0f7",
   lightText: "#f4f4f7",
   darkText: "#0E0849",
   iconColor: "#AE1E15",
+  activebtn: "#d7286e",
+  disabledbtn: "#d7286e5a",
 };
