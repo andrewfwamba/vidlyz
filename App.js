4555bdcc273ea04a10a9df9785fdb0f630406cf5
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-02-17 22:46:23 +0300

    refactor

--- App.js
+++ App.js
@@ -6,18 +6,20 @@ import { StyleSheet, Text, View } from "react-native";
 import Home from "./src/screens/Home";
 import Login from "./src/screens/Login";
 import Signup from "./src/screens/Signup";
+import MyDrawer from "./src/utils/Navigation";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Signup"
+        initialRouteName="Drawer"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Drawer" component={MyDrawer} />
       </Stack.Navigator>
     </NavigationContainer>
   );
