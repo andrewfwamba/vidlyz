d86d08d996b663e9bcaca89916d6da5bebc418e1
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-02-20 16:14:55 +0300

    Font fix

--- App.js
+++ App.js
@@ -7,13 +7,41 @@ import Home from "./src/screens/Home";
 import Login from "./src/screens/Login";
 import Signup from "./src/screens/Signup";
 import MyDrawer from "./src/utils/Navigation";
+import { useEffect, useState } from "react";
+import { useFonts } from "expo-font";
+import * as Font from "expo-font";
 
-const Stack = createNativeStackNavigator();
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    // const [fontsLoaded] = useFonts({
+    //   "Poppins-Thin": require("./assets/fonts/Poppins-Thin.ttf"),
+    // });
+    async function loadFonts() {
+      await Font.loadAsync({
+        "Poppins-Thin": require("./assets/fonts/Poppins-Thin.ttf"),
+        "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+        "Poppins-Light": require("./assets/fonts/Poppins-Light.ttf"),
+        "Poppins-Black": require("./assets/fonts/Poppins-Black.ttf"),
+        "Poppins-Italic": require("./assets/fonts/Poppins-Italic.ttf"),
+      })
+        .then((res) => {
+          console.log("Fonts Loaded!");
+          setLoaded(true);
+        })
+        .catch((Err) => {
+          setLoaded(true);
+          console.log(Err);
+        });
+    }
+
+    loadFonts();
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Drawer"
+        initialRouteName="Signup"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Login" component={Login} />
