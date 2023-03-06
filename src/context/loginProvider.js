6a5b266d335d02bbbbe7c8a2281011376b1236c8
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-03-06 19:30:49 +0300

    feat: handle login context

---
+++ src/context/loginProvider.js
@@ -0,0 +1,56 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import client from "../api/client";
+
+const LoginContext = createContext();
+
+const LoginProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [profile, setProfile] = useState({});
+  const [loginPending, setLoginPending] = useState(false);
+
+  const fetchUser = async () => {
+    setLoginPending(true);
+    const token = await AsyncStorage.getItem("token");
+    if (token !== null) {
+      // const res = await client.get("/profile", {
+      //   headers: {
+      //     Authorization: `JWT ${token}`,
+      //   },
+      // });
+      // if (res.data.success) {
+      //   setProfile(res.data.profile);
+      //   setIsLoggedIn(true);
+      // } else {
+      //   setProfile({});
+      //   setIsLoggedIn(false);
+      // }
+      // setLoginPending(false);
+    } else {
+      setProfile({});
+      setIsLoggedIn(false);
+      setLoginPending(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <LoginContext.Provider
+      value={{
+        isLoggedIn,
+        setIsLoggedIn,
+        profile,
+        setProfile,
+        loginPending,
+        setLoginPending,
+      }}
+    >
+      {children}
+    </LoginContext.Provider>
+  );
+};
+export const useLogin = () => useContext(LoginContext);
+export default LoginProvider;
