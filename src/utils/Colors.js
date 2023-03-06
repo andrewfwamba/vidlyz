bab5a7ddc4dd6c7879f8489614154e70c9287717
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-03-06 19:28:24 +0300

    minor fix

--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -1,6 +1,7 @@
 export const Colors = {
   primary: "#000",
   transparentbg: "rgba(0,0,0,0.2)",
+  playbutton: "rgba(0,0,0,0.6)",
   mainbg: "#f0f0f7",
   lightText: "#f4f4f7",
   darkText: "#0E0849",
